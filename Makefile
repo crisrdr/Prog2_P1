7252f0b1e8784e082f86c4532a24c7ea3010b5e8
crisrdr <cristina.rodriguezdelosrios@estudiante.uam.es>
2022-02-22 20:37:45 +0100

    cambios

--- Makefile
+++ Makefile
@@ -1,43 +1,52 @@
 ####################### VARIABLES #######################
 
 EXE_1 = p1_e1
-OBJ_1 = point.o
+OBJ_1 = point.o p1_e1.o
 
 EXE_2 = p1_e2
-OBJ_2 = $(OBJ_1) map.o
+OBJ_2 = point.o map.o p1_e1.o p1_e2.o
 
 GCC = gcc -c -Wall -pedantic
 
 ##################### COMANDOS MAKE #####################
 
+## GENERAL ##
+
 all: $(EXE_1) $(EXE_2)
-
-$(EXE_1): $(OBJ_1) $(EXE_1).o
-	gcc -o $(EXE_1) $(OBJ__1) $(EXE_1).o
-
-$(EXE_2): $(OBJ_2) $(EXE_2).o
-	gcc -o $(EXE_2) $(OBJ_2) $(EXE_2).o
-
-map.o: map.h map.c
-	$(GCC) map.c
-
-point.o: point.h point.c
-	$(GCC) point.c
-
-$(EXE_1).o: $(EXE_1).c
-	$(GCC) $(EXE_1).c
 
 clean:
 	rm *.o $(EXE_1) $(EXE_2)
 
+## APARTADO 1 ##
+
+$(EXE_1): $(OBJ_1)
+	gcc -o $(EXE_1) $(OBJ_1)
+	
+$(EXE_1).o: $(EXE_1).c
+	$(GCC) $(EXE_1).c
+
+point.o: point.h point.c
+	$(GCC) point.c
+
 run1:
 	./$(EXE_1)
 
-run2:
-	./$(EXE_2)
-
 val1:
 	valgrind --leak-check=full ./$(EXE_1)
+
+## APARTADO 2 ##
+
+$(EXE_2): $(OBJ_2)
+	gcc -o $(EXE_2) $(OBJ_2)
+
+$(EXE_2).o: $(EXE_2).c
+	$(GCC) $(EXE_2).c
+
+map.o: map.h map.c
+	$(GCC) map.c
+
+run2:
+	./$(EXE_2)
 
 val2:
 	valgrind --leak-check=full ./$(EXE_2)
--- p1_e2.c
+++ p1_e2.c
@@ -1,34 +1,73 @@
-#include "map.c"
+#include "map.h"
+#include "point.h"
+
+#define N_ROWS 3
+#define N_COLS 4
+
+#define Y_INPUT 1
+#define X_INPUT 1
+#define Y_OUTPUT 2
+#define X_OUTPUT 1
 
 int main (){
     Map *maps; 
-    maps = (Map*) malloc (12*sizeof(Map*));
-    printf("Map:\n");
-    maps = map_new(4,3);
+    Point *x;
+    int i, j;
 
-    map_insertPoint(maps, point_new(0,0,BARRIER));
-    map_insertPoint(maps, point_new(0,1,BARRIER));   
-    map_insertPoint(maps, point_new(0,2,BARRIER));
-    map_insertPoint(maps, point_new(1,0,BARRIER));
-    map_insertPoint(maps, point_new(1,1,INPUT));
-    map_insertPoint(maps, point_new(1,2,BARRIER));
-    map_insertPoint(maps, point_new(2,0,BARRIER));
-    map_insertPoint(maps, point_new(2,1,OUTPUT));
-    map_insertPoint(maps, point_new(2,2,BARRIER));
-    map_insertPoint(maps, point_new(3,0,BARRIER));
-    map_insertPoint(maps, point_new(3,1,BARRIER));
-    map_insertPoint(maps, point_new(3,2,BARRIER));
+    // Punto 1: Inicialización
+    if ((maps = map_new(N_ROWS,N_COLS))==NULL){     //Prueba de errores
+        printf("Run failed\n");
+        return 1;
+    }
+    printf("Map:\n%d, %d\n", N_ROWS, N_COLS);
 
-    map_print(stdout,maps);
+    // Punto 2: Inserción del laberinto
+    for(i=0; i<N_ROWS; i++){
+        for (j=0; j<N_COLS; j++){
+            if (map_insertPoint(maps, point_new(i,j,BARRIER)) == NULL){     // Formación del mapa
+                printf("Run failed\n");                                     // Prueba de errores y liberación de memoria
+                map_free(maps);
+                return 1;
+            }
+        }
+    }
+    // Inserción de input y output
 
+    if ((map_insertPoint(maps, point_new(Y_INPUT, X_INPUT, INPUT)) == NULL) || (map_insertPoint(maps, point_new(Y_OUTPUT, X_OUTPUT, OUTPUT)) == NULL)){
+        printf("Run failed\n");     
+        map_free(maps);                 // Prueba de errores y liberación de memoria
+        return 1;
+    }
+
+    // Punto 3: Impresión del mapa
+    if (map_print(stdout,maps) == -1){
+        printf("Run failed\n");
+        map_free(maps);
+        return 1;
+    }
+
+    // Punto 4: Vecinos del output
     printf("Get output neighboors:\n");
-    Point *x = point_new(2,1,OUTPUT);
+    if ((x = point_new(Y_OUTPUT, X_OUTPUT, OUTPUT)) == NULL){
+        printf("Run failed\n");
+        map_free(maps);
+        return 1;
+    }
 
-    point_print(stdout, map_getNeighboor(maps,x, RIGHT));
-    point_print(stdout, map_getNeighboor(maps,x, UP));
-    point_print(stdout, map_getNeighboor(maps,x, LEFT));
-    point_print(stdout, map_getNeighboor(maps,x, DOWN));
+    if ( 
+        (point_print(stdout, map_getNeighboor(maps,x, RIGHT)) == -1) ||
+        (point_print(stdout, map_getNeighboor(maps,x, UP)) == -1) ||
+        (point_print(stdout, map_getNeighboor(maps,x, LEFT)) == -1) ||
+        (point_print(stdout, map_getNeighboor(maps,x, DOWN)) == -1))
+    {
+        printf("Run failed\n");
+        point_free(x);
+        map_free(maps);
+        return 1;
+    }
 
-
+    point_free(x);
+    map_free(maps);
+    
     return 0;
 }
