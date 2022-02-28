ea208b15330d7b41a69104f16f81cd93918b5c8a
crisrdr <cristina.rodriguezdelosrios@estudiante.uam.es>
2022-02-28 09:55:12 +0100

    así se queda

--- p1_e2.c
+++ p1_e2.c
@@ -15,7 +15,7 @@ int main (){
 
     /* Punto 1: Inicialización */
     if ((maps = map_new(N_ROWS,N_COLS))==NULL){     /* Prueba de errores */
-        printf("Run failed\n");
+        fprintf(stdout,"Run failed\n");
         return 1;
     }
 
@@ -26,43 +26,42 @@ int main (){
             if (i==X_INPUT && j==Y_INPUT){
                 point_setSymbol(y,INPUT);
                 if ((x = map_insertPoint(maps, y)) == NULL){      
-                    printf("Run failed\n");                                
+                    fprintf(stdout,"Run failed\n");                                
                     map_free(maps);
                     return 1;
                 }
             } else if (i==X_OUTPUT && j==Y_OUTPUT){
                 point_setSymbol(y,OUTPUT);
                 if ((x = map_insertPoint(maps, y)) == NULL){       
-                    printf("Run failed\n");                                    
+                    fprintf(stdout,"Run failed\n");                                    
                     map_free(maps);
                     return 1;
                 }
             } else {
                 if ((x = map_insertPoint(maps, y)) == NULL){      
-                    printf("Run failed\n");
-                    point_free(y);                                  
-                    map_free(maps);
+                    fprintf(stdout,"Run failed\n");                  
+                    map_free(maps);   
                     return 1;
                 }    
             }   
-            point_free(y);           
+            point_free(y);        
         }
     }
 
     /* Punto 3: Impresión del mapa */
     fprintf(stdout,"Map:\n");
-                                                /* HASTA AQUI TODO BIEN */
+ 
     if (map_print(stdout,maps) == -1){
-        printf("Run failed\n");
+        fprintf(stdout,"Run failed\n");
         map_free(maps);
         return 1;
     }
 
     /* Punto 4: Vecinos del output */
+    
     fprintf(stdout,"\nGet output neighboors:\n");
     if ((x = point_new(Y_OUTPUT, X_OUTPUT, OUTPUT)) == NULL){
         printf("Run failed\n");
-        point_free(x);
         map_free(maps);
         return 1;
     }
@@ -78,10 +77,11 @@ int main (){
         map_free(maps);
         return 1;
     }
+     
     fprintf(stdout,"\n");
-
-    point_free(x);                  /* Liberación final de memoria */
-    map_free(maps);
+                        
+    point_free(x);
+    map_free(maps);                                     /* Liberación final de memoria */
     
     return 0;
 }
